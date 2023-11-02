a21810ff205283fef759e9747455d0f4350871fb
caarlosdamian <dapecarlos@gmail.com>
2023-11-02 13:16:47 -0600

    feat(navbar): added navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -17,12 +17,16 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [mode, setMode] = useState('');
   const handleThemechange = useCallback(() => {
-    if (mode !== 'dark') {
-      setMode('light');
-      document.documentElement.classList.add('light');
-    } else {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
       setMode('dark');
       document.documentElement.classList.add('dark');
+    } else {
+      setMode('light');
+      document.documentElement.classList.remove('dark');
     }
   }, [mode]);
 
