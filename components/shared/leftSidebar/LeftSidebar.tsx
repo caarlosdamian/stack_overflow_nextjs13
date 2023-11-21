746b90293d4e576aeb7fe4a30eb57ddc2af93fc4
Carlos Damian <carlos.damian@choicehotels.com>
2023-11-21 17:44:15 -0600

    feat(components): added right and left sidebar

--- components/shared/leftSidebar/LeftSidebar.tsx
+++ components/shared/leftSidebar/LeftSidebar.tsx
@@ -5,74 +5,74 @@ import { SignedOut } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import React from 'react';
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   return (
-    <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="flex h-full flex-1 flex-col gap-6">
+    <section
+      className="background-light900_dark200 light-border sticky left-0 top-0 
+    flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 
+    shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]"
+    >
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
           return (
-            <>
-              <Link
-                href={link.route}
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`${
+                isActive
+                  ? 'primary-gradient rounded-lg text-light-900'
+                  : 'text-dark300_light900'
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={20}
+                height={20}
+                className={`${isActive ? '' : 'invert-colors'}`}
+              />
+              <p
                 className={`${
-                  isActive
-                    ? 'primary-gradient rounded-lg text-light-900'
-                    : 'text-dark300_light900'
-                } flex items-center justify-start gap-4 bg-transparent p-4`}
+                  isActive ? 'base-bold' : 'base-medium'
+                } max-lg:hidden`}
               >
-                <Image
-                  src={link.imgURL}
-                  alt={link.label}
-                  width={20}
-                  height={20}
-                  className={`${isActive ? '' : 'invert-colors'}`}
-                />
-                <p
-                  className={`${
-                    isActive ? 'base-bold' : 'base-medium'
-                  } hidden lg:flex`}
-                >
-                  {link.label}
-                </p>
-              </Link>
-            </>
+                {link.label}
+              </p>
+            </Link>
           );
         })}
       </div>
       <SignedOut>
-        <div className="flex w-full flex-col gap-3">
+        <div className="flex flex-col gap-3">
           <Link href="/sing-in">
             <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              <span className="primary-text-gradient hidden lg:flex">
-                Log in
-              </span>
               <Image
-                src="/assets/icons/sign-up.svg"
-                alt="sign-up"
+                src="/assets/icons/account.svg"
+                alt="login"
                 width={20}
                 height={20}
-                className="invert-colors md:hidden"
+                className="invert-colors lg:hidden"
               />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log in
+              </span>
             </Button>
           </Link>
           <Link href="/sing-up">
             <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-              <span className="primary-text-gradient hidden lg:flex">
-                Sing up
-              </span>
               <Image
-                src="/assets/icons/avatar.svg"
-                alt="sign-up"
+                src="/assets/icons/sign-up.svg"
+                alt="sing-up"
                 width={20}
                 height={20}
-                className="invert-colors md:hidden"
+                className="invert-colors lg:hidden"
               />
+              <span className="max-lg:hidden">Sing up</span>
             </Button>
           </Link>
         </div>
---
+++ utils/index.ts
@@ -0,0 +1,65 @@
+export const dummyTopics = [
+  {
+    _id: '1',
+    title:
+      'Would it be appropriate to point out an error in another paper during a referee report?',
+  },
+  {
+    _id: '2',
+    title: 'How can an airconditioning machine exist?',
+  },
+  {
+    _id: '3',
+    title: 'Interrogated every time crossing UK Border as citizen',
+  },
+  {
+    _id: '4',
+    title: 'Low digit addition generator',
+  },
+  {
+    _id: '5',
+    title: 'What is an example of 3 numbers that do not make up a vector?',
+  },
+];
+export const dummyTools = [
+  {
+    _id: 'Javascript',
+    name: 'Javascript',
+    totalQuestions: 20152,
+  },
+  {
+    _id: 'Next.js',
+    name: 'Next.js',
+    totalQuestions: 18493,
+  },
+  {
+    _id: 'React.js',
+    name: 'React.js',
+    totalQuestions: 16269,
+  },
+  {
+    _id: 'Node.js',
+    name: 'Node.js',
+    totalQuestions: 15121,
+  },
+  {
+    _id: 'Python',
+    name: 'Python',
+    totalQuestions: 14431,
+  },
+  {
+    _id: 'Microsoft Azure',
+    name: 'Microsoft Azure',
+    totalQuestions: 9429,
+  },
+  {
+    _id: 'PostgreSql',
+    name: 'PostgreSql',
+    totalQuestions: 9429,
+  },
+  {
+    _id: 'Machine Learning',
+    name: 'Machine Learning',
+    totalQuestions: 9429,
+  },
+];
