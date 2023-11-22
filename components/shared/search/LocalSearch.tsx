380aefc9aad0d820579487011d127b41f00e5eac
Carlos Damian <carlos.damian@choicehotels.com>
2023-11-22 16:29:34 -0600

    feat(home): added local search bar component

---
+++ components/shared/search/LocalSearch.tsx
@@ -0,0 +1,56 @@
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import { Input } from '@/components/ui/input';
+
+interface Props {
+  route: string;
+  iconPosition: 'left' | 'right';
+  imgSrc: string;
+  placeholder: string;
+  otherClasses?: string;
+}
+
+const LocalSearch = ({
+  iconPosition,
+  imgSrc,
+  placeholder,
+  route,
+  otherClasses,
+}: Props) => {
+  return (
+    <div className="relative w-full max-w-[600px] max-lg:hidden">
+      <div
+        className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4 ${otherClasses}`}
+      >
+        {iconPosition === 'left' && (
+          <Image
+            src={imgSrc}
+            alt="search"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+        <Input
+          type="text"
+          placeholder={placeholder}
+          value={''}
+          onChange={() => {}}
+          className="paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none"
+        />
+        {iconPosition === 'right' && (
+          <Image
+            src={imgSrc}
+            alt="search"
+            width={24}
+            height={24}
+            className="cursor-pointer"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default LocalSearch;
