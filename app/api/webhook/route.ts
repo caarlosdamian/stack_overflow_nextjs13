33b9e23136617aa7d50ea6cdb282053bc602b846
Carlos Damian <carlos.damian@choicehotels.com>
2024-04-16 16:50:15 -0600

    remove consolelog

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -30,7 +30,6 @@ export async function POST(req: Request) {
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
- console.log('++++++payload++++',payload)
   // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
