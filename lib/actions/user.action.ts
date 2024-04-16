a21544e5a092de75e853db099c9acb36edd77c88
Carlos Damian <carlos.damian@choicehotels.com>
2024-04-16 13:03:04 -0600

    fix(app)

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -79,7 +79,7 @@ export async function deleteUser(params: DeleteUserParams) {
     await Question.deleteMany({ author: user._id });
 
     // TODO: delete user answers, comments, etc.
-
+    // @ts-ignore
     const deletedUser = await User.findByIdAndDelete(user._id);
 
     return deletedUser;
