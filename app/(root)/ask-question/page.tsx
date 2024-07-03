3628d258ee5b1bc0124088adc92a291aa6e0a155
caarlosdamian <dapecarlos@gmail.com>
2024-07-03 14:31:49 -0600

    fix(auth): fix ask question id error

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,8 +1,8 @@
-import Question from '@/components/forms/Question'
+import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 
 const Page = async () => {
   const { userId } = auth();
@@ -15,11 +15,13 @@ const Page = async () => {
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
-      <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)}/>
-      </div>
+      {mongoUser && (
+        <div className="mt-9">
+          <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
